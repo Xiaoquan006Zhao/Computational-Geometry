81b18523281b9e6832fb81a251af9b28127b4993
Xiaoquan006Zhao <45891316+Xiaoquan006Zhao@users.noreply.github.com>
2020-06-06 14:53:37 -0500

    Create GrahamScan.java

---
+++ Convex-hull-2d/GrahamScan.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+/**
+ * Computing convex hull in 2d space by implementing Graham Scan. The essence of this algorithm is first sort the input by 
+ * polar angle wrt. to a selected point. Then apply the insight that a segment is part of the CH iff all points lie on one
+ * side. To check if a point lie on the right side or the left side, one can use cross product.
+ *
+ * This algorithm takes O (n logn) dominated by the pre-sorting. 
+ */
+
+public class GrahamScan {
+    private arrayList<point> points = new arrayList<point>(); //should be sorted.
+    private stack<point> ch = new stack<point>();
+    
+    //find the reference point that is used as the pivot for calculating polar angle. 
+    //the lowest point (smallest y-coord), if ties, using the point with smallest x-coord.
+    private point findReference (arrayList<point> inputPoints){
+        point reference = inputPoints.get(0);
+        point cur = null;
+        
+        for(int i = 1; i < inputPoints.size(); i++){
+            cur = inputPoints.get(i);
+            if (cur.y < reference.y)
+                reference = cur;
+            else if (cur.y == reference.y && cur.x < reference.x)
+                reference = cur;
+        }
+        
+        return reference;
+    }
+    
+    //assuming the input points are all distinct.
+    public GrahamScan (arrayList<point> inputPoints) {
+        if(inputPoints.size == 0)
+            throw new Exception("empty input");
+        
+        point reference = findReference(inputPoints);
+        
+        Collections.sort(inputPoints, )
+    }
+
+}
