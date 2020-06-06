36e0b097abcc5110642b4f986d8dae44ee869a25
Xiaoquan006Zhao <45891316+Xiaoquan006Zhao@users.noreply.github.com>
2020-06-06 14:02:30 -0500

    Update Graham-Scan.java

--- Convex-hull-2d/Graham-Scan.java
+++ Convex-hull-2d/Graham-Scan.java
@@ -1,1 +1,17 @@
 //Author: Xiaoquan Zhao
+
+/**
+ * Computing convex hull in 2d space by implementing Graham Scan. The essence of this algorithm is first sort the input by 
+ * polar angle wrt. to a selected point. Then apply the insight that a segment is part of the CH iff all points lie on one
+ * side. To check if a point lie on the right side or the left side, one can use cross product.
+ *
+ * This algorithm takes O (n logn) dominated by the pre-sorting. 
+ */
+
+public class GrahamScan {
+  
+  
+  public GrahamScan () {
+    
+
+}
