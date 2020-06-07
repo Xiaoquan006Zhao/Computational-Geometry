41ce54d675aa4ff223f0ab58287fa4481d72e84d
Xiaoquan006Zhao <45891316+Xiaoquan006Zhao@users.noreply.github.com>
2020-06-06 20:06:27 -0500

    Update GrahamScan.java

--- Convex-hull-2d/GrahamScan.java
+++ Convex-hull-2d/GrahamScan.java
@@ -5,6 +5,8 @@ import java.util.*;
  * polar angle wrt. to a selected point. Then apply the insight that a segment is part of the CH iff all points lie on one
  * side. To check if a point lie on the right side or the left side, one can use cross product.
  *
+ * output represent by vertices of the convex hull in clockwise order.
+ *
  * This algorithm takes O (n logn) dominated by the pre-sorting. 
  */
 
