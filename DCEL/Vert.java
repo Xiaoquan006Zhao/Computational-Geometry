7b94cdc443a767db0b1fcfdf690aff96a5664c10
Xiaoquan006Zhao <45891316+Xiaoquan006Zhao@users.noreply.github.com>
2020-06-09 21:16:24 -0500

    Create Vert.java

---
+++ DCEL/Vert.java
@@ -0,0 +1,30 @@
+
+/***
+ * This class is used to represent vertex in DCEL.
+ */
+
+public class Vert extends Point{
+    public final double x;
+    public final double y;
+	  public final String name;
+	  private Edge incidentEdge;
+	  
+	  Vert(String name, double x, double y){
+			 this.x = x;
+       this.y = y;
+			 this.name = name;
+			 incidentEdge = null;
+	  }
+	  
+	  public Edge getIncident() {
+			 return incidentEdge;
+	  }
+	  
+	  public void setIncident(Edge e) {
+			 incidentEdge = e;
+	  }
+	  
+	  public String toString (){
+			 return name + " (" + x + ", " + y + ") ";
+	  }
+}
