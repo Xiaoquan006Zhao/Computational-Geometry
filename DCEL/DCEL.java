928c21af7b7eefd18f785ff80063dcfc99331630
Xiaoquan006Zhao <45891316+Xiaoquan006Zhao@users.noreply.github.com>
2020-06-09 21:55:35 -0500

    Update DCEL.java

--- DCEL/DCEL.java
+++ DCEL/DCEL.java
@@ -4,5 +4,5 @@ import java.util.ArrayList;
 public class DCEL {
 	  public ArrayList<Vert> vertices = new ArrayList<Vert>();
 	  public ArrayList<Edge> edges = new ArrayList<Edge>();
-	  public Edge[] faces;
+	  public ArrayList<Face> faces = new ArrayList<Face>();
 }
