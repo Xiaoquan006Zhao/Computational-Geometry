abd6e0b1ec3cc30faec80e16c260934a3751519d
Xiaoquan006Zhao <45891316+Xiaoquan006Zhao@users.noreply.github.com>
2020-06-09 22:02:21 -0500

    Update Face.java

--- DCEL/Face.java
+++ DCEL/Face.java
@@ -1,4 +1,32 @@
 
-public class Face {
+import java.util.ArrayList;
 
+/***
+ * This class is used to represent edge in DCEL.
+ */
+ 
+public class Face {
+    private Edge outer;
+    private ArrayList<Edge> inner = new ArrayList<Edge>();
+    
+    public Face (Edge outer, ArrayList<Edge> inner){
+        this.outer = outer;
+        this.inner = inner;
+    }
+    
+    public Edge getOuter(){
+        return outer;
+    }
+    
+    public void setOuter(Edge outer){
+        this.outer = outer;
+    }
+    
+    public ArrayList<Edge> getInner(){
+        return inner;
+    }
+    
+    public void setInner(ArrayList<Edge> inner){
+        this.inner = inner;
+    }
 }
