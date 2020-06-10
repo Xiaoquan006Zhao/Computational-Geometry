fa97c135f51d08aad80a171c6f3ec9bcab718be6
Xiaoquan006Zhao <45891316+Xiaoquan006Zhao@users.noreply.github.com>
2020-06-09 21:13:08 -0500

    Update and rename   point.java to point.java

--- Convex-hull-2d/point.java
+++ Convex-hull-2d/point.java
@@ -1,7 +1,5 @@
 // this class is used to represent points in 2d space and referenced in the convex-hull computing classes.
 
-
-
 public class point {
     //once a point is initialized there is no need to modify it further more in future programs. 
     //Thus make the varables "final".
